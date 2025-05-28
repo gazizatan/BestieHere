4bd0c3f78fb37f6ee93a949aa2e2b6c633db63fd
qazintelligence <beebee800800@gmail.com>
2025-05-28 23:30:13 +0500

    check

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return MaterialApp(
       title: 'BestieHere',
       debugShowCheckedModeBanner: false,
